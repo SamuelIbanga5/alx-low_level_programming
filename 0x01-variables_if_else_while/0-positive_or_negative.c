86ea3bfd2630f6232cb786dfd88afc5dc459c7b5
Samuel Ibanga <ibangajnr27@gmail.com>
2022-09-09 16:57:55 +0100

    Added betty style in documentation

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
 /**
- * main - Entry point.
- *
+ * main - Entry point
  * Description: This program checks if the random value of n
- * is positive(greater than 0) or negative(less than 0) or 
- * equal to 0 and prints the block of code in the condition.
- *
- * Return: 0 (Success).
+ * is positive (greater than 0) or negative (less than 0) or
+ * equal to 0 and prints the block of code in the condition
+ * Return: 0 (Success)
  */
 int main(void)
 {
@@ -19,7 +14,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -28,9 +23,10 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	}
-	else if (n < 0)
+	else
 	{
 		printf("%d is negative\n", n);
 	}
+
 	return (0);
 }
