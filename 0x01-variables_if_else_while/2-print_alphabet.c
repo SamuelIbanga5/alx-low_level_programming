87496700d5922399e9ed34a20a7f1d32d1e1facb
Samuel Ibanga <ibangajnr27@gmail.com>
2022-09-09 20:59:42 +0100

    Created C source code file 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Program Entry point.
+ * Description: This program prints the alphabets in lowercase and
+ * then in uppercase followed by a new line.
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
