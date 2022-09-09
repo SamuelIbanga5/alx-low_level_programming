d6393a24a1a2cb69a1f3f75ba8f290ed505ab9d5
Samuel Ibanga <ibangajnr27@gmail.com>
2022-09-10 00:58:27 +0100

    Created C source code file 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Program entry point.
+ * Description: This program prints all possible
+ * combinations of single-digit numbers.
+ * Return: 0 Success.
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 100)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
