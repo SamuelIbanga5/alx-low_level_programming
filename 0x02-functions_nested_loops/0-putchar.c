cb03fb32f8a26ea30a48d30e81b7c876fafabe7c
Samuel Ibanga <ibangajnr27@gmail.com>
2022-09-13 16:19:48 +0100

    Created C source code file 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Program Entry point.
+ * Description: This program prints "_putchar" without
+ * the standard library functions printf, puts etc...
+ * Return: Always 0 Success.
+ */
+int main(void)
+{
+	char string[] = "_putchar";
+	
+	int i;
+	i = 0;
+	while (string[i] != '\0')
+	{
+		_putchar(string[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
