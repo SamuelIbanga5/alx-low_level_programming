ae2324c9ff0eb5b3fe5efa255f9bdd1a46965d96
Samuel Ibanga <ibangajnr27@gmail.com>
2022-09-13 23:17:28 +0100

    Created C source code file 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_last_digit - Program that prints the last digit of a number.
+ * @n: Number to check and print last digit.
+ *
+ * Return: Always 0 Success.
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+	if (n < 0)
+	{
+		last_digit = -1 * (n % 10);
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+	else
+	{
+		last_digit = n % 10;
+		_putchar(last_digit + '0');
+	}
+	return (0);
+}
