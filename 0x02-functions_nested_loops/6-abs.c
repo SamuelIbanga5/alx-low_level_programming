7d50136ecdf5f1d4c12b666aa99aefbc9461aa42
Samuel Ibanga <ibangajnr27@gmail.com>
2022-09-13 19:29:19 +0100

    Created C source code file 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs(int) - Program that computes the absolute value of an integer.
+ * @n: Number to convert to absolute value.
+ * Return: Always 0 Success.
+ */
+int _abs(int n)
+{
+	int retval;
+
+	if (n >= 0)
+	{
+		retval = n;
+	}
+	else
+	{
+		retval = n * -1;
+	}
+	return (retval);
+}
