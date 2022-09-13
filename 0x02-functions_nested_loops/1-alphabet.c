bea15932a94ce2e87f75baaa53e40d2fd3c88de0
Samuel Ibanga <ibangajnr27@gmail.com>
2022-09-13 16:51:12 +0100

    Created C source code file 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,6 @@
  * Description: This program prints all the alphabets
  * in lowercase followed by a new line without using standard
  * library functions.
- * Return: Always 0 Success.
  */
 void print_alphabet(void)
 {
@@ -16,5 +15,4 @@ void print_alphabet(void)
 		ch++;
 	}
 	_putchar('\n');
-	return;
 }
