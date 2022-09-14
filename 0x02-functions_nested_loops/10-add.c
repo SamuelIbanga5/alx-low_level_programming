a2cafde5ac962672ecc404265599958179d51363
Samuel Ibanga <ibangajnr27@gmail.com>
2022-09-14 01:32:39 +0100

    Created C source code file 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Program that adds two integers and returns result.
+ * @a: First operand
+ * @b: second operand to be added.
+ *
+ * Return: result after addition.
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+	return (result);
+}
