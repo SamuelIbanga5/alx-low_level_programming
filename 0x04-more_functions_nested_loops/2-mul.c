24609f1215c3cfa0c90947b28877b47790dee203
Samuel Ibanga <ibangajnr27@gmail.com>
2022-09-15 16:38:44 +0100

    Created C source code file 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - Program that multiplies two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: result.
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
