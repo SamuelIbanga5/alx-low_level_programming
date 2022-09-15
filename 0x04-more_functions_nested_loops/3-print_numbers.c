399760bd1e46e4066c3a346f2c2d85692b090336
Samuel Ibanga <ibangajnr27@gmail.com>
2022-09-15 16:45:44 +0100

    Created C source code file 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - Program that prints numbers from 0 to 9.
+ */
+void print_numbers(void)
+{
+	int numbers;
+	for (numbers = 0; numbers <= 9; numbers++)
+	{
+		_putchar(numbers + '0');
+	}
+	_putchar('\n');
+}
