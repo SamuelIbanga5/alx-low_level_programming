d416db6a4940afc8d54f2e57535b443fdecbe905
Samuel Ibanga <ibangajnr27@gmail.com>
2022-09-15 17:58:04 +0100

    Created C source code file 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - Program that prints a straight line.
+ */
+void print_line(int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
+		i++;
+	}
+	_putchar('\n');
+}
