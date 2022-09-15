a3580b22d748d39ff7abc730952a4f1ca4797d48
Samuel Ibanga <ibangajnr27@gmail.com>
2022-09-15 16:56:13 +0100

    Updated main.h and created source code file 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - Program that prints numbers 0 throught 9
+ * except number 2 and 4.
+ */
+void print_most_numbers(void)
+{
+	int numbers;
+	for (numbers = 0; numbers <= 9; numbers++)
+	{
+		if (numbers != 2 && numbers != 4)
+		{
+			_putchar(numbers + '0');
+		}
+	}
+	_putchar('\n');
+}
