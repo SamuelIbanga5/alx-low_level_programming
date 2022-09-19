504cca6ca2f384c9029b77bbf80b4ef6d468ca5d
Oliveann0 <jamesblessinganie1@gmail.com>
2022-09-19 19:56:25 +0100

    Created project directory for pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Program that takes a pointer to an int
+ * as a parameter and updates the value it points to to 98.
+ * @n: Pointer to change value.
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
