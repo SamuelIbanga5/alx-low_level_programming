147553faa6fd7d6ed0d8f870d9f5407c8092442c
Samuel Ibanga <ibangajnr27@gmail.com>
2022-09-20 00:37:13 +0100

    Updated C source code file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - Program that takes a pointer to an int
  * as a parameter and updates the value it points to to 98.
- * @n: Pointer to change value.
+ * @n: Pointer to change value
  *
  */
 void reset_to_98(int *n)
