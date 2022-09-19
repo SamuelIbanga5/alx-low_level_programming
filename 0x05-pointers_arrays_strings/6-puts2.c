b9a6b92ba137ac67912a106029923a06ca82ffa9
Oliveann0 <jamesblessinganie1@gmail.com>
2022-09-19 23:12:10 +0100

    Created C source code file 6-puts.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts - Program that prints every other character or a string
+ * puts2 - Program that prints every other character or a string
  * starting with the first character followed by a new line.
  * @str: String to print characters.
  *
@@ -12,8 +12,11 @@ void puts2(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
 	}
 	_putchar('\n');
 }
