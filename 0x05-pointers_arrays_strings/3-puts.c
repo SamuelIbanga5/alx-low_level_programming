36b43b4a6dfb193fa687493392e76c9c7f007582
Oliveann0 <jamesblessinganie1@gmail.com>
2022-09-19 21:12:02 +0100

    Created C source code file 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - Program that prints a string followed by a new line
+ * to "stdout".
+ * @str: String to print.
+ *
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
