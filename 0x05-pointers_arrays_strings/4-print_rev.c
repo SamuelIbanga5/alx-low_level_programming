4148070f546eaf645a9a3ae01e0fe80a6542bd64
Oliveann0 <jamesblessinganie1@gmail.com>
2022-09-19 22:21:16 +0100

    Updated 4-print_rev.c to betty coding style

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,20 +6,18 @@
  */
 void print_rev(char *s)
 {
-	int length;
 	int i;
 
-	length = 0;
 	i = 0;
 	while (*(s + i) != '\0')
 	{
-		length += 1;
 		i++;
 	}
-	while (length >= 0)
+	i--;
+	while (i >= 0)
 	{
-		_putchar(*(s + length));
-		length--;
+		_putchar(*(s + i));
+		i--;
 	}
 	_putchar('\n');
 }
