8a35d5062e1b93636ab759bbd5280699704f0e83
Samuel Ibanga <ibangajnr27@gmail.com>
2022-09-20 00:38:11 +0100

    Updated C source code file

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,7 @@
 /**
  * swap_int - Program that swaps the values of two integers.
  * @a: First integer to swap.
- * @b: Second integer to swap.
+ * @b: Second integer to swap
  *
  */
 void swap_int(int *a, int *b)
