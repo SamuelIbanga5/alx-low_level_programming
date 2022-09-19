3bd422c7e2680b49fc48c3467e5402fd40f49b78
Samuel Ibanga <ibangajnr27@gmail.com>
2022-09-20 00:42:02 +0100

    Updated C source code file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 /**
  * puts2 - Program that prints every other character or a string
  * starting with the first character followed by a new line.
- * @str: String to print characters.
+ * @str: String to print characters
  *
  */
 void puts2(char *str)
