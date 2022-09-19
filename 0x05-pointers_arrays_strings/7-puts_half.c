ae8ecd7ae2b3ac1b28fc94e8964dfc267d406bc0
Oliveann0 <jamesblessinganie1@gmail.com>
2022-09-19 23:27:53 +0100

    Created C source code file 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - Program that prints half of a string
+ * followed by a string.
+ * @str: String to check.
+ *
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	i = (i / 2) + 1;
+	while (str[i] > '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
