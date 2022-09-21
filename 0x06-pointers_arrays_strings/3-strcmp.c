415096329e286daca68e9555580a0525b6a24134
Samuel Ibanga <ibangajnr27@gmail.coom>
2022-09-21 22:24:42 +0100

    Created C source code file 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _strcmp - Function that compares two strings.
+ * @s1: First string.
+ * @s2: Second string.
+ *
+ * Return: "<0" if "s1" is less than "s2" else
+ * "0" if "s1" is equal to "s2" else ">0" if "s1"
+ * is greater than "s2".
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int str1, str2, i, retval;
+
+	str1 = 0;
+	str2 = 0;
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		str1 += (int)s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		str2 += (int)s2[i];
+		i++;
+	}
+	if (str1 < str2)
+	{
+		retval = -15;
+	}
+	else if (str1 == str2)
+	{
+		retval = 0;
+	}
+	else
+	{
+		retval = 15;
+	}
+	return (retval);
+}
