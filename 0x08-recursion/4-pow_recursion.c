161696852b0bc3558dc8138638b9eed887bb8208
Samuel Ibanga <ibangajnr27@gmail.com>
2022-09-28 20:53:34 +0100

    Created C source code file 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * pow_recursion - Function that returns the value of x raised to the power of y.
+ * @x: Number.
+ * @y: Power.
+ * Return: Result of the power of a number.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
