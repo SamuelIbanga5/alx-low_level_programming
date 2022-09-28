d015eeec65a17547b88bf9f8eee26dafcd4806d0
Samuel Ibanga <ibangajnr27@gmail.com>
2022-09-28 18:48:34 +0100

    Created C source file 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Function that prints a string in reverse.
+ * @s: String to print in reverse.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
