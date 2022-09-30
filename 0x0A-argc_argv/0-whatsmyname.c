c32299a9a09bac0a6c6a25cc26f180a548771510
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-01 00:45:33 +0100

    Created new project directory 0x0A-argc_Argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Program entry point.
+ * Description - Prints name of the executable file.
+ * Return: Always 0 for success.
+ */
+int main(int __attribute__((unused))argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
