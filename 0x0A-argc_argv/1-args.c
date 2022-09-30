7821802c070a3b1cac48700ea553c9c3fb686e2e
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-01 00:51:24 +0100

    Created C source code file 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Program that prints the number of arguments passed into it.
+ * @argc: Number of command line arguments.
+ * @argv: Array of command line arguments.
+ * Return: Always 0 Success.
+ */
+int main(int argc, char __attribute__((unused))*argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
