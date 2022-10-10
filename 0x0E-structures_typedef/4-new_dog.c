6099169da9bc1e2c93f73ef19f7891bc2b191d31
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-10 19:15:00 +0100

    Updated C source code file 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,8 +23,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	i++;
 	new_dog->name = malloc(sizeof(char) * i);
 	if (new_dog->name == NULL)
+	{
 		free(new_dog);
 		return (NULL);
+	}
 	for (k = 0; k < i; k++)
 		new_dog->name[k] = name[k];
 	new_dog->age = age;
@@ -33,9 +35,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 	j++;
 	new_dog->owner = malloc(sizeof(char) * j);
 	if (new_dog->owner == NULL)
+	{
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
+	}
 	for (k = 0; k < j; k++)
 		new_dog->owner[k] = owner[k];
 	return (new_dog);
