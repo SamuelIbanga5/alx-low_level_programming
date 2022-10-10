28dc3321216de19dbca5786ef49d9997a534f41c
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-10 12:11:00 +0100

    Created C source code file 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - Function that prints a struct dog.
+ * @d: Pointer to type struct dog.
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+	
+	}
+	else if (d -> name == NULL)
+	{
+		printf("Name: (nil)");
+	}
+	else if (d -> owner == NULL)
+	{
+		printf("Owner: (nil)");
+	}
+	printf("Name: %s\n", d -> name);
+	printf("Age: %f\n", d -> age);
+	printf("Owner: %s\n", d -> owner);
+}
