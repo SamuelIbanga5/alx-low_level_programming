9664aa0ac7e57a51f7475c9d51a1ebee1ea2143c
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-10 11:53:43 +0100

    Created C source code file 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - Function that initialize a variable of type struct dog.
+ * @d: Pointer to struct dog.
+ * @name: Name of dog.
+ * @age: Age of dog.
+ * @owner: Owner of dog.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}
