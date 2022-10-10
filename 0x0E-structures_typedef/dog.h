a7838bf9facce9fe0eb79ae71b5b440fd3cce811
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-10 11:41:57 +0100

    Created project directory for Structures and TypeDef 0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Structure that shows different members.
+ * @name: Name of dog.
+ * @age: Age of dog.
+ * @owner: Owner of the dog.
+ *
+ * Description: This structure defines attributes of a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
