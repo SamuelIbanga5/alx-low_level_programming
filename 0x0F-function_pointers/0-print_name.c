975cd9f787573df56dbe2df292a42c9d0bfd25fa
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-14 21:03:30 +0100

    Updated C source code file 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stddef.h>
+
 /**
  * print_name - Function that prints a name.
  * @name: Name to print.
@@ -7,8 +7,5 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-	{
-		f(name);
-	}
+	f(name);
 }
