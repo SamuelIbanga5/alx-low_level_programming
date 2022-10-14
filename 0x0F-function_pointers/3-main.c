5a3cc28942699de25ea619f91c6325d67311065b
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-15 00:20:11 +0100

    Created 3-main.c file for test

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+/**
+ * main - Function to check the code.
+ * @argc: Argument count.
+ * @argv: Arguments in use.
+ * Return: Always 0 success.
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", operation(a, b));
+	return (0);
+}
