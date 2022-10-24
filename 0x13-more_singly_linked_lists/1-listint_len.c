c9c18e509ecbeefb36b9676eca59ad8e94d4a0f9
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-24 09:37:49 +0100

    Created C source code file 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - Function that returns the number of elements in a linked list.
+ * @h: Pointer to listint_t structure.
+ * Return: Number of elements in Linked list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
