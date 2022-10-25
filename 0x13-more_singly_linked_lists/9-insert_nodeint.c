eba683e4c4661a57704a32280f37de8c1e20bf06
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-25 15:56:02 +0100

    Created C source code file 9-insert_nodeint_at_index.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - Function that inserts the node
+ * at index of a listint_t linked list.
+ * @head: Pointer to listint_t structure.
+ * @index: Index of element.
+ * Return: 1 if it succeeded else -1 if failed.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *current, *new;
+
+	if (head == NULL)
+		return (NULL);
+	if (idx != 0)
+	{
+		current = *head;
+		for (i = 0; i < idx - 1 && current != NULL; i++)
+		{
+			current = current->next;
+		}
+		if (current == NULL)
+			return (NULL);
+	}
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	new->next = current->next;
+	current->next = new;
+	return (new);
+}
