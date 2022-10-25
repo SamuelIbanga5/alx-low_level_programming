418970602dd84f675774c28760e880ab80ec8a85
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-25 15:57:23 +0100

    Updated C source code file 9-insert_nodeint_at_index.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,7 +3,8 @@
  * insert_nodeint_at_index - Function that inserts the node
  * at index of a listint_t linked list.
  * @head: Pointer to listint_t structure.
- * @index: Index of element.
+ * @idx: Index of element.
+ * @n: Number to add to list.
  * Return: 1 if it succeeded else -1 if failed.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
