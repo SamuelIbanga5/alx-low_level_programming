180746d5dcb8d0e341ad1c7a6069f2e57ff19e66
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-27 17:41:09 +0100

    Created new project directory for Bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * binary_to_unit - Function that converts a binary to an unsigned int.
+ * @b: Pointing to a string of Zero and one characters.
+ * Return: COnverted number.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int num;
+
+	num = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+	}
+	return (num);
+}
