0ae142fec1a0098dbb25b5c48ddf8cf8cf428fe5
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-27 17:42:35 +0100

    Updated C file to betty standard

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * binary_to_unit - Function that converts a binary to an unsigned int.
+ * binary_to_uint - Function that converts a binary to an unsigned int.
  * @b: Pointing to a string of Zero and one characters.
  * Return: COnverted number.
  */
