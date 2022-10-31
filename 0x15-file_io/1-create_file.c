179c20c78440fe3efa4e80cbc45cad7202be9ded
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-31 16:57:26 +0100

    Created C source code file 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _strlen - Length of string.
+ * @str: Pointer to characters(String).
+ * Return: Length of the string.
+ */
+size_t _strlen(char *str)
+{
+	size_t i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * create_file - Function that creates a file.
+ * @filename: Name of the file.
+ * @text_content: COntent of the file.
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+		len = write(fd, text_content, _strlen(text_content));
+	close(fd);
+	if (len == -1)
+		return (-1);
+	return (1);
+}
