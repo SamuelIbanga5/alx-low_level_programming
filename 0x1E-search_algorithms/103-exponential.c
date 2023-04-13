500554f307e0d7df10c92eda2d7df6205112cbde
Samuel Ibanga <ibangajnr27@gmail.com>
2023-04-13 09:03:58 +0100

    time complexity of jump search algorithm

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - Searches for a value in a sorted array
+ * using Exponential search algorithm.
+ * @array: A pointer to the first element if the array to search in.
+ * @size: The number of elements in array.
+ * @value: Value to search for.
+ * Return: The first index where value is located.
+ */
+
+int exponential_search(int *array, size_t size, int value)
+{
+
